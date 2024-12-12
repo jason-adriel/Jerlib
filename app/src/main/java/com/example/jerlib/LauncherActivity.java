050bc31fc4eb3543198a0badc1d82178099806bf
Jason Adriel <143072575+subtle64@users.noreply.github.com>
2024-12-12 23:04:29 +0700

    Google Firebase Setup with Auth and Firestore.

---
+++ app/src/main/java/com/example/jerlib/LauncherActivity.java
@@ -0,0 +1,41 @@
+package com.example.jerlib;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.jerlib.utils.Auth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = Auth.instance.getCurrentUser();
+        if (currentUser == null) {
+            Intent invalidAuth = new Intent(this, AuthActivity.class);
+            this.startActivity(invalidAuth);
+            finish();
+        } else {
+            //TODO Connect with Home
+        }
+    }
+}
