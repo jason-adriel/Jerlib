0f3b3e9d3c888d461bc585dd8a9cc8db6273452a
Jason Adriel <143072575+subtle64@users.noreply.github.com>
2025-01-10 16:17:30 +0700

    Fixed shelf implementation. Optimized API calls to a single API call per shelf update.

--- app/src/main/java/com/example/jerlib/fragments/ShelfFragment.java
+++ app/src/main/java/com/example/jerlib/fragments/ShelfFragment.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,12 +15,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.jerlib.R;
 import com.example.jerlib.adapters.ScopusAdapter;
-import com.example.jerlib.models.Author;
-import com.example.jerlib.models.Bibjson;
 import com.example.jerlib.models.Entry;
-import com.example.jerlib.models.Journal;
-import com.example.jerlib.models.Link;
-import com.example.jerlib.models.Subject;
+import com.example.jerlib.models.ResultResponse;
+import com.example.jerlib.services.ApiService;
 import com.example.jerlib.utils.FirebaseUtil;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -30,11 +28,24 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ShelfFragment extends Fragment {
     String shelfID;
     List<String> articles;
     List<Entry> listData = new ArrayList<>();
     RecyclerView shelfItemRV;
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://doaj.org/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    ApiService apiService = retrofit.create(ApiService.class);
+    TextView shelfNotificationTV;
     private void fetchUserShelf() {
         FirebaseFirestore db = FirebaseUtil.db();
         String uuid = FirebaseUtil.auth.getUid();
@@ -61,34 +72,33 @@ public class ShelfFragment extends Fragment {
                                         listData.clear();
                                         articles = (List<String>) document.get("entries");
                                         assert articles != null;
-                                        for (String id : articles) {
-                                            Link link = new Link();
-                                            link.setUrl("https://google.com");
-                                            Author author = new Author();
-                                            author.setName("-");
-                                            Entry entry = new Entry();
-                                            Bibjson bibjson = new Bibjson();
-                                            Journal j = new Journal();
-                                            j.setTitle("-");
-                                            Subject sb = new Subject();
-                                            sb.setTerm("-");
-                                            bibjson.setKeywords(List.of("-"));
-                                            bibjson.setLink(List.of(link));
-                                            bibjson.setTitle("Article");
-                                            bibjson.setAuthor(List.of(author));
-                                            bibjson.setYear("2024");
-                                            bibjson.setJournal(j);
-                                            bibjson.setAbstract("-");
-                                            bibjson.setSubject(List.of(sb));
-                                            entry.setBibjson(bibjson);
-                                            entry.setId(id);
-                                            listData.add(entry);
+                                        if (!articles.isEmpty()) {
+                                            shelfNotificationTV.setText("");
+                                            String queryString = getQueryString();
+                                            fetchScopusData(queryString);
+                                        } else if (shelfItemRV.getAdapter() != null) {
+                                            shelfItemRV.getAdapter().notifyDataSetChanged();
+                                            shelfNotificationTV.setText("Your recently emptied your shelf! Go search for more now!");
+                                        } else {
+                                            shelfNotificationTV.setText("Your shelf is empty, go search for some references!");
                                         }
-                                        ScopusAdapter adapter = new ScopusAdapter(listData);
-                                        shelfItemRV.setAdapter(adapter);
-                                        adapter.notifyDataSetChanged();
                                     }
                                 }
+                            }
+
+                            @NonNull
+                            private String getQueryString() {
+                                StringBuilder query = new StringBuilder();
+                                query.append("id:(");
+                                for (int i = 0; i < articles.size(); ++i) {
+                                    query.append(articles.get(i));
+                                    if (i != articles.size() - 1) {
+                                        query.append(" OR ");
+                                    }
+                                }
+                                query.append(")");
+                                String queryString = query.toString();
+                                return queryString;
                             }
                         });
 
@@ -123,7 +133,51 @@ public class ShelfFragment extends Fragment {
         fetchUserShelf();
         shelfItemRV = view.findViewById(R.id.shelfItemRV);
         shelfItemRV.setLayoutManager(new LinearLayoutManager(getContext()));
-
+        shelfNotificationTV = view.findViewById(R.id.shelfNotificationTV);
         return view;
+    }
+
+    private void fetchScopusData(String query) {
+        Log.i("ApiService", "Sending API request...");
+
+        // Perform the request
+        Call<ResultResponse> call = apiService.getResponse(query, "created_date:desc", 1);
+
+        call.enqueue(new Callback<>() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onResponse(Call<ResultResponse> call, Response<ResultResponse> response) {
+                Log.i("ApiService", "Response received: " + response.toString());
+                if (response.isSuccessful()) {
+                    ResultResponse resultResponse = response.body();
+                    assert resultResponse != null;
+                    listData = resultResponse.getEntries();
+                    ScopusAdapter adapter = new ScopusAdapter(listData);
+                    shelfItemRV.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                    Log.i("ApiService", "Scopus Data: " + listData);
+                } else {
+                    Log.e("ApiService", "Request failed: " + response.errorBody());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResultResponse> call, Throwable t) {
+                // Log the failure message
+                Log.e("ApiService", "Request failed: " + t.getMessage(), t);
+
+                // You can also check for different types of errors
+                if (t instanceof java.net.UnknownHostException) {
+                    // No internet connection
+                    Log.e("ApiService", "No internet connection: " + t.getMessage());
+                } else if (t instanceof java.net.SocketTimeoutException) {
+                    // Timeout occurred
+                    Log.e("ApiService", "Request timed out: " + t.getMessage());
+                } else {
+                    // Other types of errors
+                    Log.e("ApiService", "Unexpected error: " + t.getMessage());
+                }
+            }
+        });
     }
 }
